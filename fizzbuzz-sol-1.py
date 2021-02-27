7e8caddd44f23275d480aa49e276dc61dadac4f8
nsn22 <72639425+nsn22@users.noreply.github.com>
2021-02-27 11:43:05 +0700

    fizzbuzz-sol-1.1.py

--- fizzbuzz-sol-1.py
+++ fizzbuzz-sol-1.py
@@ -1,9 +1,11 @@
 for i in range(1,101):
-    if(i % 5 == 0 and i % 3 == 0):
-        print("FizzBuzz")
+    if(i % 5 == 0 and i % 3 == 0 and i % 7):
+        print("FizzBuzzOke")
     elif(i % 3 == 0):
         print("Fizz")
     elif(i % 5 == 0):
         print("Buzz")
+    elif(i % 7 == 0):
+        print("Oke")
     else:
         print(i)
